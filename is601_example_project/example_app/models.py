4af80473ac930349e2a29a1638f3aea992d21f9a
mourtallah <serigne.faye@icloud.com>
2022-10-03 18:53:12 -0400

    add BakedGood model to app models

--- is601_example_project/example_app/models.py
+++ is601_example_project/example_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class BakedGood(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    good_type = models.CharField(
+        max_length=2,
+        choices=[("BR", "Bread"), ("CA", "Cake"), ("CO", "Cookie"), ("PR", "Pretzel")],
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    recipe = models.TextField()
